3cf00a5c7fcad4bd432164862e06d8f1957d4f52
valeri <valerisergeev98@gmail.com>
2022-11-08 22:14:15 +0100

    Работа со списками

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 // import React, {useState} from 'react';
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import {useState} from "react";
+import PostList from "./components/PostList";
+
 function App() {
-  // const [value, setValue] = useState(' ТЕКСТ В ИНПУТЕ ');
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Javascript 2', body: 'Description'},
+        {id: 3, title: 'Javascript 3', body: 'Description'},
+    ])
 
   return (
     <div className="App">
-        <Counter/>
+        <PostList posts={posts} title={'Список постов 1'}/>
     </div>
   );
 }
