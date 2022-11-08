8d1277c9c58ee1f30d1f28d3f677573690bea8dc
valeri <valerisergeev98@gmail.com>
2022-11-08 23:00:34 +0100

    хук useRef один управляемый и один неуправляемый инпут

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css'
 import PostItem from "./components/PostItem";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -13,8 +15,33 @@ function App() {
         {id: 3, title: 'Javascript 3', body: 'Description'},
     ])
 
+    const [title, setTitle] = useState('')
+    const bodyInputRef = useRef()
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        console.log(title);
+        console.log(bodyInputRef.current.value)
+    }
+
   return (
     <div className="App">
+        <form>
+            {/* Управляемый компонент */}
+            <MyInput
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                type="text"
+                placeholder="Название поста"
+            />
+            {/* Неуправляемый/неконтролируемый компонент */}
+            <MyInput
+                ref = {bodyInputRef}
+                type="text"
+                placeholder="Описание поста"
+            />
+            <MyButton onClick={addNewPost}>Добавить пост</MyButton>
+        </form>
         <PostList posts={posts} title={'Список постов 1'}/>
     </div>
   );
